63122ba096b6380a01b13c01d023a7c371d28be0
MiltonValenzuela <miltvalenz@hotmail.com>
2019-12-03 01:16:03 -0400

    Add User model and createUser Endpoint

--- server/src/models/user/schema.js
+++ server/src/models/user/schema.js
@@ -10,6 +10,14 @@ const schema = new Schema({
   email: {
     type: String,
     required: [true]
+  },
+  password: {
+    type: String,
+    required: [true]
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
 
---
+++ server/src/routes/users/createUser.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator/check');
+
+/* GET users listing. */
+module.exports = ({ User }) => {
+	router.post(
+		'/register',
+		[
+			check('name').isLength({ min: 3 }),
+			check(
+				'email',
+				'The password must be 5+ chars long and contain a number'
+			).isEmail(),
+			check('password').isLength({ min: 8 }),
+			check('passwordConfirmation').isLength({ min: 8 }),
+			check('email').custom(value => {
+				return User.findByEmail(value).then(user => {
+					if (user) {
+						return Promise.reject('E-mail already in use');
+					}
+				});
+			}),
+			check('password').custom((value, { req }) => {
+				if (value !== req.body.passwordConfirmation) {
+					throw new Error('Password confirmation is incorrect');
+				}
+			})
+		],
+		async (req, res, next) => {
+			try {
+				const {
+					name,
+					email,
+					password,
+					passwordConfirmation
+				} = req.body;
+
+				const newUser = new User({
+					name,
+					email,
+					password
+				});
+                console.log(newUser);
+				await newUser.save();
+
+				res.status(200).json({ User: newUser });
+			} catch {
+				next(error);
+			}
+		}
+	);
+
+	return router;
+};
