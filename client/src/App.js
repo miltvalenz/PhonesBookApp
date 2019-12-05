0f41e631c1b7ea7780ad98e598a12937617457a5
MiltonValenzuela <miltvalenz@hotmail.com>
2019-12-05 19:30:43 -0400

    Login component working

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import LogIn from './components/LogIn/LogIn';
-import Contact from './components/Contact/Contact';
-import Dashboard from './components/Contact/Contact';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="/Login" component={Login} />
-				<Route path="/Signup" component={Reg} />
+				<Route exact path="/Login" component={LogIn} />
 			</Switch>
 			<Switch>
 				<Route path="/Dashboard" component={Dashboard} />
--- client/src/components/LogIn/LogIn.js
+++ client/src/components/LogIn/LogIn.js
@@ -1,62 +1,106 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+import React, { Component } from 'react';
 import './LogIn.css';
 
-const LogIn = () => {
-	const [email, setEmail] = useState('');
-
-	const [password, setPassword] = useState('');
-
-	const onSubmit = event => {
-		event.preventDefault();
-
-		axios({
+import {
+	Button,
+	Card,
+	CardBody,
+	CardGroup,
+	Col,
+	Container,
+	Form,
+	Input,
+	InputGroup,
+	InputGroupAddon,
+	InputGroupText,
+	Row
+} from 'reactstrap';
+class Login extends Component {
+	constructor() {
+		super();
+		this.state = {
+			Email: '',
+			Password: ''
+		};
+		this.Password = this.Password.bind(this);
+		this.Email = this.Email.bind(this);
+		this.login = this.login.bind(this);
+	}
+	Email(event) {
+		this.setState({ Email: event.target.value });
+	}
+	Password(event) {
+		this.setState({ Password: event.target.value });
+	}
+	login(event) {
+		console.log(this.state.Email + ' ' + this.state.Password);
+		fetch('http://localhost:4000/api/v1/users/login', {
 			method: 'post',
-			url: 'http://localhost:4000/api/v1/users/login',
-			data: {
-				email,
-				password
-			}
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				email: this.state.Email,
+				password: this.state.Password
+			})
 		})
-			.then(response => console.log(response))
+			.then(Response => Response.json())
+			.then(result => {
+				console.log(result);
+				if (result.Status == 'Unauthorized') alert('Invalid User');
+				else this.props.history.push('/Dashboard');
+			})
 			.catch(error => console.log(error));
-	};
-
-	return (
-		<div className="joinOuterContainer">
-			<div className="joinInnerContainer">
-				<h1 className="heading">LogIn</h1>
-				<div>
-					<input
-						placeholder="Email"
-						className="joinInput"
-						type="text"
-						onChange={event => setEmail(event.target.value)}
-					/>
-				</div>
-				<div>
-					<input
-						placeholder="Password"
-						className="joinInput mt-20"
-						type="text"
-						onChange={event => setPassword(event.target.value)}
-					/>
-				</div>
-				<Link
-					onClick={event =>
-						!email || !password ? onSubmit(event) : null
-					}
-					to={`/user?user=${email}`}
-				>
-					<button className="button" type="submit">
-						Sign In
-					</button>
-				</Link>
+	}
+	render() {
+		return (
+			<div className="app flex-row align-items-center">
+				<Container>
+					<Row className="justify-content-center">
+						<Col md="9" lg="7" xl="6">
+							<CardGroup>
+								<Card className="p-2">
+									<CardBody>
+										<Form>
+											<div
+												className="row"
+												className="mb-2 pageheading"
+											>
+												<div className="col-sm-12 btn btn-primary">
+													Login
+												</div>
+											</div>
+											<InputGroup className="mb-3">
+												<Input
+													type="text"
+													onChange={this.Email}
+													placeholder="Enter Email"
+												/>
+											</InputGroup>
+											<InputGroup className="mb-4">
+												<Input
+													type="password"
+													onChange={this.Password}
+													placeholder="Enter Password"
+												/>
+											</InputGroup>
+											<Button
+												onClick={this.login}
+												color="success"
+												block
+											>
+												Login
+											</Button>
+										</Form>
+									</CardBody>
+								</Card>
+							</CardGroup>
+						</Col>
+					</Row>
+				</Container>
 			</div>
-		</div>
-	);
-};
-
-export default LogIn;
+		);
+	}
+}
+export default Login;
