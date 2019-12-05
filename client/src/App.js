eb73750c8dc0ddf0554b89cdf5a3853970f89654
MiltonValenzuela <miltvalenz@hotmail.com>
2019-12-05 16:20:55 -0400

    Add dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import LogIn from './components/LogIn/LogIn';
 import Contact from './components/Contact/Contact';
+import Dashboard from './components/Contact/Contact';
 
 function App() {
 	return (
 		<Router>
-			<Route path="/" exact component={LogIn} />
-			<Route path="/contacts" component={Contact} />
+			<Switch>
+				<Route exact path="/Login" component={Login} />
+				<Route path="/Signup" component={Reg} />
+			</Switch>
+			<Switch>
+				<Route path="/Dashboard" component={Dashboard} />
+			</Switch>
 		</Router>
 	);
 }
