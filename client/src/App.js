71b70277718b226a097432c18f9f26aa069de67c
MiltonValenzuela <miltvalenz@hotmail.com>
2020-03-13 19:38:56 -0400

    new front

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import './App.css';
-import Register from './components/Register/Register';
-import LogIn from './components/LogIn/LogIn';
-import Dashboard from './components/Dashboard/Dashboard';
-import Contact from './components/Contact/Contact';
-import ContactRegister from './components/ContactRegister/ContactRegister';
-import ContactList from './components/ContactList/ContactList';
+import React from 'react';import './App.css';
 
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/Login" component={LogIn} />
-				<Route exact path="/Register" component={Register} />
-				<Route exact path="/Contacts/details" component={Contact} />
-				<Route exact path="/Contacts" component={ContactList} />
-				<Route exact path="/Contacts/Create" component={ContactRegister} />
-			</Switch>
-			<Switch>
-				<Route path="/Dashboard" component={Dashboard} />
-			</Switch>
-		</Router>
+		<div>
+			<h1>Hello world</h1>
+		</div>
 	);
 }
 
